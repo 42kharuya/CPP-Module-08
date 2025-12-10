5e80e9d7d00426579cc61fcb45864cfddcf35221
42kharuya <haruya.0411.k@gmail.com>
2025-12-10 15:02:04 +0900

    ex02/main.cppにMutantStackのテストを追加し、元のスタックの動作を確認するコードを実装

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,10 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
+	std::cout << "------- MutantStack -------" << std::endl;
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -16,6 +18,7 @@ int main()
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
@@ -25,7 +28,42 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::stack<int> s(mstack);
+
+	std::cout << "------- Original Stack -------" << std::endl;
+	std::list<int> s;
+
+	s.push_back(5);
+	s.push_back(17);
+	std::cout << s.back() << std::endl;
+
+	s.pop_back();
+	std::cout << s.size() << std::endl;
+
+	s.push_back(3);
+	s.push_back(5);
+	s.push_back(737);
+	s.push_back(0);
+
+	std::list<int>::iterator sIt = s.begin();
+	std::list<int>::iterator sIte = s.end();
+	++sIt;
+	--sIt;
+	while (sIt != sIte)
+	{
+		std::cout << *sIt << std::endl;
+		++sIt;
+	}
 	return 0;
 }
-//実行結果がふさわしいか家に帰って照らし合わせる
+
+// Stack(LIFO)
+
+// ------- Push Check -------
+// stack{5, 17} <-
+// top = 17
+// stack{5} ->
+// size = 1
+// stack{5,3,5,737,0} <-
+
+// ------- Iterator Check -------
+// begin <-(-) {5,3,5,737,0} ->(+) end
